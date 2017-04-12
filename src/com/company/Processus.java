3047fd9a0f6a235e4e2705f72da8c044fab2a2ea
oussama <oussamabonnor@gmail.com>
2017-04-12 18:11:13 +0100

    Uploading project assets (already in making progress)

---
+++ src/com/company/Processus.java
@@ -0,0 +1,67 @@
+package com.company;
+
+/**
+ * Created by Oussama on 11/04/2017.
+ */
+public class Processus {
+    private int arriveTime;
+    private int cpuTime;
+    private int priority;
+    private int name;
+    private boolean passed;
+
+    public int getName() {
+        return name;
+    }
+
+    public void setName(int name) {
+        this.name = name;
+    }
+
+    public Processus(int name, int arriveTime, int cpuTime, int priority, boolean passed) {
+        this.arriveTime = arriveTime;
+        this.cpuTime = cpuTime;
+        this.priority = priority;
+        this.name = name;
+        this.passed = passed;
+    }
+
+    public boolean isPassed() {
+        return passed;
+    }
+
+    public void setPassed(boolean passed) {
+        this.passed = passed;
+    }
+
+    public int getArriveTime() {
+        return arriveTime;
+    }
+
+    public void setArriveTime(int arriveTime) {
+        this.arriveTime = arriveTime;
+    }
+
+    public int getCpuTime() {
+        return cpuTime;
+    }
+
+    public void setCpuTime(int cpuTime) {
+        this.cpuTime = cpuTime;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
+
+
+    public void processing(int timeToProcess) {
+        setCpuTime(getCpuTime() - timeToProcess);
+
+    }
+
+}
